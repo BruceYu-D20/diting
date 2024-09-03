852a1b361b5bc3cc996964cd60eb6dc023897ca2
BruceYu-D20 <bluesyu163@163.com>
2024-09-03 11:23:26 +0800

    增加eval的进程数

--- eval/whisper_eval.py
+++ eval/whisper_eval.py
@@ -1,3 +1,5 @@
+import os
+
 import evaluate
 from peft import PeftModel
 from transformers import WhisperForConditionalGeneration, WhisperProcessor
@@ -14,14 +16,16 @@ from tools.DataCollatorSpeechSeq2SeqWithPadding import DataCollatorSpeechSeq2Seq
 # 获取所有的数据读写路径
 paths = path_with_datesuffix()
 
+# 获取所有的checkpoint dir
+checkpoint_dirs = [d for d in os.listdir(paths['MODEL_OUT_DIR']) if os.path.isdir(os.path.join(paths['MODEL_OUT_DIR'], d))]
+
 base_model = WhisperForConditionalGeneration.from_pretrained(
         paths['MODEL_PATH'],
         low_cpu_mem_usage=True,
         torch_dtype=torch.float32, # 在ct2转换时，如果指定的--quantization float16，这里必须时float16
         device_map="auto"
     )
-# model = PeftModel.from_pretrained(base_model, os.path.join(paths['MODEL_OUT_DIR'], paths['PEFT_MODEL_ID']))
-model = PeftModel.from_pretrained(base_model, "E:/code/whisper_ft/model_out/20240828.29311/checkpoint-2")
+
 processor = WhisperProcessor.from_pretrained(paths['MODEL_PATH'], language='ar', task='transcribe')
 feature_extractor = processor.feature_extractor
 tokenizer = processor.tokenizer
@@ -44,34 +48,52 @@ common_voice = common_voice.map(_prepare_dataset, remove_columns=common_voice.co
 
 # 拼装DataLoader
 data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
-eval_dataloader = DataLoader(common_voice["test"], batch_size=8, collate_fn=data_collator)
+# 使用4个进程同时eval
+eval_dataloader = DataLoader(common_voice["test"], batch_size=50, collate_fn=data_collator, num_workers=4)
 
 # 加载wer metrics
 metric_wer = evaluate.load(os.path.join(paths['METRICS_PATH'], "wer"))
+metric_cer = evaluate.load(os.path.join(paths['METRICS_PATH'], "cer"))
 
-# 模型评估
-model.eval()
-for step, batch in enumerate(tqdm(eval_dataloader)):
-    with torch.cuda.amp.autocast():
-        with torch.no_grad():
-            generated_tokens = (
-                model.generate(
-                    input_features=batch["input_features"].to('cuda'),
-                    decoder_input_ids=batch["labels"][:, :4].to('cuda'),
-                    max_new_tokens=255,
+# 如果当前目录下cr.txt存在，就删除
+if os.path.exists('er.txt'):
+    os.remove('er.txt')
+
+for checkpoint_dir in checkpoint_dirs:
+    # model
+    # 加载本地checkpoint，此dir是peft的参数
+    model = PeftModel.from_pretrained(base_model, os.path.join(paths['MODEL_OUT_DIR'], checkpoint_dir))
+    # 模型评估
+    model.eval()
+    for step, batch in enumerate(tqdm(eval_dataloader)):
+        with torch.cuda.amp.autocast():
+            with torch.no_grad():
+                generated_tokens = (
+                    model.generate(
+                        input_features=batch["input_features"].to('cuda'),
+                        decoder_input_ids=batch["labels"][:, :4].to('cuda'),
+                        max_new_tokens=255,
+                    )
+                    .cpu()
+                    .numpy()
                 )
-                .cpu()
-                .numpy()
-            )
-            labels = batch["labels"].cpu().numpy()
-            labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-            decoded_preds = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
-            decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-            metric_wer.add_batch(
-                predictions=decoded_preds,
-                references=decoded_labels,
-            )
-    del generated_tokens, labels, batch
-    gc.collect()
-wer = 100 * metric_wer.compute()
-print(f"{wer=}")
+                labels = batch["labels"].cpu().numpy()
+                labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+                decoded_preds = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+                decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+                metric_wer.add_batch(
+                    predictions=decoded_preds,
+                    references=decoded_labels,
+                )
+                metric_wer.add_batch(
+                    predictions=decoded_preds,
+                    references=decoded_labels,
+                )
+        del generated_tokens, labels, batch
+        gc.collect()
+    wer = 100 * metric_wer.compute()
+    cer = 100 * metric_cer.compute()
+    # 将{"wer": wer, "cer": cer}的结果写入到当前目录下 er.txt中
+    with open("er.txt", "a") as f:
+        f.write(f"{checkpoint_dir} {wer} {cer}\n")
+    print({"wer": wer, "cer": cer})
