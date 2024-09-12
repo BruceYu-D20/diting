51ee4445be3a211d18748afa2caaa482498caa18
BruceYu-D20 <bluesyu163@163.com>
2024-09-12 09:25:18 +0800

    '增加获取数据类的接口'

---
+++ api/data_process.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from typing import Union
+from datasets import DatasetDict, Dataset
+
+
+class data_process:
+    def fetch_data(data_path) -> Union[DatasetDict, Dataset]:
+        pass
+
