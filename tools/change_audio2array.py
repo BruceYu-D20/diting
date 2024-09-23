bdbc46dc4bd1cf5e681c7f64b881023ec823c9ce
BruceYu-D20 <bluesyu163@163.com>
2024-09-23 16:36:51 +0800

    修改工具类注释

--- tools/change_audio2array.py
+++ tools/change_audio2array.py
@@ -7,43 +7,8 @@ from datasets import load_dataset, DownloadMode
 import datacsv_check
 
 '''
-业务上，微调的数据要求提供：语音文件和语音文件对应的文本文件
-
-业务上，语音文件存储在
-/data/audio/{languege}/
-|-- batch_1.xlxs
-|-- batch_1/
-    |-- audio_1.wav
-    |-- audio_2.wav
-    |-- audio_3.wav
-    |-- ......
-|-- batch_2.xlsx
-|-- batch_2/
-    |-- audio_4.wav
-    |-- audio_5.wav
-    |-- audio_6.wav
-    |-- ......
-
-eg.
-在/data/audio/en 目录下，有batch_1.xlsx文件
-要求填写: 字段1：语音文件的路径 字段2：语音文件对应的文字 字段3：采样率
-在/data/audio/en/batch_1/ 目录下，有audio_1.wav、audio_2.wav、audio_3.wav等语音文件
-/data/audio/en/batch_1/audio_1.wav  'this is a script process original audio' 16000
-/data/audio/en/batch_1/audio_2.wav  'jingle bell jingle bell jingle all the way' 16000
-/data/audio/en/batch_1/audio_3.wav  'this is peppa pig' 16000
-
-说明：
-1. 每个batch文件夹，在存储完成后，运行转化脚本，会将audio的array存储到新的文件夹中。
-所以，batch一旦执行脚本，请不要在向文件夹中添加新的语音文件。
-如果有新的语音文件，请新建一个目录，将新的语音文件存到里面
-2. 根据excel去找文件夹，所以excel和文件夹要有相同的名称，且在同一目录下
-3. 文件夹下所有excel对应的文件夹都会被处理，所以excel修改好之后，再放到对应目录下
-
-执行流程：
-1. 获取到当前目录下的excel文件（xlxs格式）
-
-输出的音频：
-path Audio sentence locale
+将音频文件处理为datasets.features.Audio
+规则看README.md的2.3和2.4章节
 '''
 
 # def parse_toolconfig(config_path):
